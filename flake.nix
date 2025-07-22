d087dca2cd080ed7999b9d3573cadc125f0cd950
cheesecakecat <peppohappy8@gmail.com>
2025-07-22 16:43:38 +0300

    dev: nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A lightweight wrapper to zig-ify SDL3.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, zig-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ zig-overlay.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ zig-overlay.packages.${system}.master ];
+        };
+      });
+}
