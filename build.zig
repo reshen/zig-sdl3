427b36013e38b54c34952fad6c08f7da0360d2f8
cheesecakecat <peppohappy8@gmail.com>
2025-07-28 09:27:02 +0300

    build: build sdl_net

--- build.zig
+++ build.zig
@@ -76,6 +76,8 @@ pub fn build(b: *std.Build) !void {
     extension_options.addOption(bool, "main", sdl3_main);
     const ext_image = b.option(bool, "ext_image", "Enable SDL_image extension") orelse false;
     extension_options.addOption(bool, "image", ext_image);
+    const ext_net = b.option(bool, "ext_net", "Enable SDL_net extension") orelse false;
+    extension_options.addOption(bool, "net", ext_net);
 
     // Linking zig-sdl to sdl3, makes the library much easier to use.
     sdl3.addOptions("extension_options", extension_options);
@@ -83,11 +85,91 @@ pub fn build(b: *std.Build) !void {
     if (ext_image) {
         setupSdlImage(b, sdl3, sdl_dep_lib, c_sdl_preferred_linkage, cfg);
     }
+    if (ext_net) {
+        setupSdlNet(b, sdl3, sdl_dep_lib, c_sdl_preferred_linkage, cfg);
+    }
 
     _ = setupDocs(b, sdl3);
     _ = setupTest(b, cfg, extension_options);
     _ = try setupExamples(b, sdl3, cfg);
     _ = try runExample(b, sdl3, cfg);
+}
+
+pub fn setupSdlNet(b: *std.Build, sdl3: *std.Build.Module, sdl_dep_lib: *std.Build.Step.Compile, linkage: std.builtin.LinkMode, cfg: std.Build.TestOptions) void {
+    const target = cfg.target orelse b.standardTargetOptions(.{});
+    const optimize = cfg.optimize;
+
+    const upstream = b.lazyDependency("sdl_net", .{}) orelse return;
+    const native_os = target.result.os.tag;
+
+    const lib_name = "SDL3_net";
+    const version = std.SemanticVersion.parse("3.0.0") catch unreachable;
+    // Options.
+    const shared = b.option(bool, "shared", "Build a shared library") orelse false;
+    const werror = b.option(bool, "werror", "Treat warnings as errors") orelse false;
+    // Library.
+    const lib = b.addLibrary(.{
+        .name = lib_name,
+        .version = version,
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
+        .linkage = linkage,
+    });
+
+    var lib_c_flags: std.ArrayList([]const u8) = .init(b.allocator);
+    defer lib_c_flags.deinit();
+    lib_c_flags.appendSlice(&.{"-std=c99"}) catch @panic("OOM");
+    if (werror) {
+        lib_c_flags.append("-Werror") catch @panic("OOM");
+    }
+
+    lib.root_module.addCSourceFile(.{
+        .file = upstream.path("src/SDL_net.c"),
+        .flags = lib_c_flags.items,
+    });
+    // Headers.
+    lib.root_module.addIncludePath(upstream.path("include"));
+    // Defines.
+    lib.root_module.addCMacro("BUILD_SDL", "1");
+    lib.root_module.addCMacro("SDL_BUILD_MAJOR_VERSION", b.fmt("{d}", .{version.major}));
+    lib.root_module.addCMacro("SDL_BUILD_MINOR_VERSION", b.fmt("{d}", .{version.minor}));
+    lib.root_module.addCMacro("SDL_BUILD_MICRO_VERSION", b.fmt("{d}", .{version.patch}));
+    if (shared and native_os == .windows) {
+        lib.root_module.addCMacro("DLL_EXPORT", "");
+    }
+    if (native_os != .windows and native_os != .haiku) {
+        lib.root_module.addCMacro("_DEFAULT_SOURCE", "");
+    }
+    lib.root_module.linkLibrary(sdl_dep_lib);
+    // Linking.
+    if (native_os == .windows) {
+        lib.root_module.linkSystemLibrary("iphlpapi", .{});
+        lib.root_module.linkSystemLibrary("ws2_32", .{});
+        if (shared) {
+            lib.root_module.addWin32ResourceFile(.{ .file = upstream.path("version.rc") });
+        }
+    } else if (native_os == .haiku) {
+        lib.root_module.linkSystemLibrary("network", .{});
+    }
+    // Linker version.
+    if (target.result.ofmt == .elf or target.result.ofmt == .macho) {
+        lib.setVersionScript(upstream.path("src/SDL_net.sym"));
+    }
+    if (shared) {
+        lib.linker_allow_shlib_undefined = false;
+    }
+
+    b.installArtifact(lib);
+    // Installation.
+    const install_header = b.addInstallHeaderFile(upstream.path("include/SDL3_net/SDL_net.h"), "SDL3_net/SDL_net.h");
+    b.getInstallStep().dependOn(&install_header.step);
+    const install_license = b.addInstallFile(upstream.path("LICENSE.txt"), "share/licenses/SDL3_net/LICENSE.txt");
+    b.getInstallStep().dependOn(&install_license.step);
+
+    sdl3.linkLibrary(lib);
 }
 
 // Most of this is copied from https://github.com/allyourcodebase/SDL_image/blob/main/build.zig.
